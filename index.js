b3cd81285b4ca89ab66c4dbb233c31b9e6bfcf26
Miné E <mine_esen@yahoo.ca>
2019-05-22 04:24:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var name = "Susan"
 var height = "74"
-if (name==="Susan"){
-  console.log("The name is correct.")
-}
-else{
-  console.log("Expected name is Susan.")
-}
 
 
 // Don't worry about this
